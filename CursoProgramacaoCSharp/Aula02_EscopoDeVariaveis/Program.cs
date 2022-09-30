fe571e8f5650357b583b92c62e78cb66194d40f3
Eliezer Alves <eliezeralvesbernardino@gmail.com>
2022-09-30 12:25:14 -0300

    Aula 02 Escopo de Variáveis

---
+++ CursoProgramacaoCSharp/Aula02_EscopoDeVariaveis/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+class Aula02
+{
+    static int num = 10;
+    static void Main(){
+        int num2 = 0;
+        Console.WriteLine(num);
+    }
+    void teste(){
+        int num = 0;
+        Console.WriteLine(num);
+    }
+}
