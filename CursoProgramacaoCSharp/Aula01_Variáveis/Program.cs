2c41ef293bc695f767ffabff59cf31e88ecc99b4
Eliezer Alves <eliezeralvesbernardino@gmail.com>
2022-09-30 12:09:27 -0300

    Aula 01 Variáveis

---
+++ CursoProgramacaoCSharp/Aula01_Variáveis/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+class Aula01
+{
+    static void Main(){
+        byte   n1     =  10;
+        int    num    =  0;
+        char   letra  =  'c';
+        float  valor  =  5.3f;
+        string nome   =  "Joel";
+
+        var aux = nome;
+        Console.WriteLine(aux);
+
+
+        int num1, num2, res;
+        num1 = 10;
+        num2 = 2;
+        res = num1 + num2;
+
+        Console.WriteLine($"A soma de {num1} + {num2} é igual á {res}");
+    }
+}
