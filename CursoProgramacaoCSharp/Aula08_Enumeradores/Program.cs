0a8be1300757a3a00f6f09a308f14bdca4ad1a85
Eliezer Alves <eliezeralvesbernardino@gmail.com>
2022-10-01 12:16:29 -0300

    Enumeradores (enum)

---
+++ CursoProgramacaoCSharp/Aula08_Enumeradores/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+class Aula08
+{
+    enum DiaSemana{Domingo,Segunda,Terça,Quarta,Quinta,Sexta,Sábado};
+    static void Main(){
+        DiaSemana ds = DiaSemana.Domingo;
+        DiaSemana sm = (DiaSemana)3;
+
+        Console.WriteLine(ds);
+        Console.WriteLine(sm);
+
+        int days = (int)DiaSemana.Sexta;
+        Console.WriteLine(days);
+    }
+}
