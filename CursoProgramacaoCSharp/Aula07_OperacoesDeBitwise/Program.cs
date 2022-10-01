2f243b655d9ca3c30c11a2966057f9fce81d4628
Eliezer Alves <eliezeralvesbernardino@gmail.com>
2022-10-01 11:58:15 -0300

    Operações de Bitwise

---
+++ CursoProgramacaoCSharp/Aula07_OperacoesDeBitwise/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+class Aula07{
+    static void Main(){
+        int num = 2;
+        num = num << 1;
+        Console.WriteLine(num);
+    }
+}
