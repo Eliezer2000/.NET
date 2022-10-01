94e6914bae2a12b3a4605e568f4413b3be695e90
Eliezer Alves <eliezeralvesbernardino@gmail.com>
2022-10-01 18:38:03 -0300

    Switch Case

---
+++ CursoProgramacaoCSharp/Aula13_SwitchCase/Program.cs
@@ -0,0 +1,36 @@
+﻿// See https://aka.ms/new-console-template for more information
+class Aula13
+{
+    static void Main(){
+
+        int tempo = 0;
+        char escolha;
+
+        Console.WriteLine("Rio de Janeiro/RJ a São Paulo/SP");
+        Console.WriteLine("Escolha o transporte: [A] Avião [C] Carro  [O] Onibus");
+        escolha = char.Parse(Console.ReadLine());
+
+        switch(escolha){
+            case 'a':
+            case 'A':
+                tempo = 50;
+                break;
+            case 'c':
+            case 'C':
+                tempo = 480;
+                break;
+            case 'o':
+            case 'O':
+                tempo = 660;
+                break;
+            default:
+                tempo = -1;
+                break;
+        }
+        if(tempo < 0){
+            Console.WriteLine("Transporte indisponível");
+        }else{
+            Console.WriteLine($"Para o transporte escolhido o tempo é: {tempo} minutos.");
+        }
+    }
+}
