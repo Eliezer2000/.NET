042f3d21fa4d7e279a409bfff27d532b7559ad0e
Eliezer Alves <eliezeralvesbernardino@gmail.com>
2022-10-01 10:12:05 -0300

    Operadores e Operações

---
+++ CursoProgramacaoCSharp/Aula03_Operadores_Operacoes/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+class Aula03{
+
+    static void Main(){
+        int res = (10 + 5) * 2;
+        Console.WriteLine(res);
+
+        bool n = 10 < 5;
+        Console.WriteLine(n);
+
+        int num = 10;
+        num += 2;
+        Console.WriteLine(num);
+
+        bool n1 = (5 < 3) | (10 > 5);
+        Console.WriteLine(n1);
+
+        bool n2 = (5 > 3) & (10 > 5);
+        Console.WriteLine(n2);
+    }
+}
+
+    
+
