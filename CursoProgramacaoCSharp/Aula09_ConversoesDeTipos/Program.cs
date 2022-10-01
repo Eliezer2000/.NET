8b766547e407c8d9f1ce3d92b65c2cfff1f206b9
Eliezer Alves <eliezeralvesbernardino@gmail.com>
2022-10-01 12:36:53 -0300

    Conversões de tipos (typecast)

---
+++ CursoProgramacaoCSharp/Aula09_ConversoesDeTipos/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+class Aula09
+{
+    static void Main(){
+
+        int   n1 = 10;
+        float n2 = n1;
+        Console.WriteLine(n2);
+
+        float n3  = 10.5f;
+        int   n4  = (int) n3; //type cast
+        Console.WriteLine(n4);
+
+        int vInt = 10;
+        short vShort = (short) vInt;
+        Console.WriteLine(vShort);
+    }
+}
