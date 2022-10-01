e40b346c10f6ea7b6278127b33531c27e8953b01
Eliezer Alves <eliezeralvesbernardino@gmail.com>
2022-10-01 11:05:34 -0300

    Constantes em C#

---
+++ CursoProgramacaoCSharp/Aula05_ConstantesEmC#/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+class Aula05{
+    static void Main(){
+        const string txt = "texto";
+        const double pi = 3.1415;
+
+        Console.WriteLine($"txt {txt}\nPi: {pi}");
+    }
+}
