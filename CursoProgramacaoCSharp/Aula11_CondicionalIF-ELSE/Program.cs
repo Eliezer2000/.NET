7f2b34e267c6534b487432374454220bde669cd7
Eliezer Alves <eliezeralvesbernardino@gmail.com>
2022-10-01 17:42:32 -0300

    Condicional IF-ELSE

---
+++ CursoProgramacaoCSharp/Aula11_CondicionalIF-ELSE/Program.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+class Aula11
+{
+    static void Main(){
+        int n1, n2, n3, n4, res;
+        res = n1 = n2 = n3 = n4 = 0;
+        string resultado = "Reprovado";
+
+        Console.WriteLine("Digite sua nota 1: ");
+        n1 = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Digite a nota 2: ");
+        n2 = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Digite a nota 3: ");
+        n3 = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Digite a nota 4: ");
+        n4 = Convert.ToInt32(Console.ReadLine());
+
+        res = n1 + n2 + n3 + n4; 
+
+        if(res < 40){
+            resultado = "Reprovado";
+        }else if(res < 60){
+            resultado = "Recuperação";
+        }else{
+            resultado = "Aprovado";
+        }
+        
+        Console.WriteLine($" Nota: {res}. Resultado: {resultado}");
+    }
+}
