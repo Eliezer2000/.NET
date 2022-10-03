2fb6c126dde440fdabc3c8408769481209c9af0f
Eliezer Alves <eliezeralvesbernardino@gmail.com>
2022-10-03 16:05:42 -0300

    Loop ForEach estruturas de iteração

---
+++ CursoProgramacaoCSharp/Aula20_LoopForEach/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+class Aula20{
+    static void Main(){
+        int[] num = new int[3] {22,11,77};
+        
+        for(int i = 0; i < num.Length; i ++){
+            Console.WriteLine(num[i]);
+        }
+
+
+        int[] n = new int[3] {11,22,33};
+
+        foreach(int x in n){
+            Console.WriteLine(x);
+        }
+    }
+}
