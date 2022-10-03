fd26f689a396eb8395fc1773909f1109d3217bce
Eliezer Alves <eliezeralvesbernardino@gmail.com>
2022-10-03 14:11:47 -0300

    Loop For Estruturas de iteração

---
+++ CursoProgramacaoCSharp/Aula17_LoopForEstruturaDeIteracao/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+class Aula17
+{
+    static void Main(){
+
+        int[] num = new int[10];
+
+        for(int i = 0; i < num.Length; i ++){
+            num[1] = i;
+            
+        }
+        for(int i = 0; i < 10; i ++){
+            Console.WriteLine($"Valor na posição {i}: {num[i]} ");
+        }
+
+    }
+}
