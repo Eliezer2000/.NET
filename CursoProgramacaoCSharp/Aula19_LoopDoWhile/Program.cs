59f9c189e8ba9af2dd2601e854af188fcaf958de
Eliezer Alves <eliezeralvesbernardino@gmail.com>
2022-10-03 15:31:31 -0300

    Loop DO While estruturas de iteração

---
+++ CursoProgramacaoCSharp/Aula19_LoopDoWhile/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+class Aula19
+{
+    static void Main(){
+        
+        string senha = "123";
+        string senhaUser;
+        int tentativas = 0;
+        
+
+        do{
+            Console.Clear();
+            Console.WriteLine("Digite a senha: ");
+            senhaUser = Console.ReadLine();
+            tentativas ++;
+        }
+        while(senha != senhaUser);
+        Console.Clear();
+        Console.WriteLine($"Senha correta, tentativas: {tentativas}");
+    }
+}
