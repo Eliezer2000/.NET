b664949ff62d17f806f6f65bcb32d746505b0e79
Eliezer Alves <eliezeralvesbernardino@gmail.com>
2022-10-03 14:59:03 -0300

    Loop While estruturas de iteração

---
+++ CursoProgramacaoCSharp/Aula18_LoopWhileEstruturaDeIteracao/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+class Aula18
+{
+    static void Main(){
+        int[] num = new int[10];
+
+        int i = num.Length -1;
+        while(i > 0){
+            num[i] = 0;
+            Console.WriteLine(num[1]);
+            i--;
+        }
+        Console.WriteLine("Fim do loop");
+    }
+}
