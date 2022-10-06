09446fdda3b250c8b408b80a7b79900126213981
Eliezer Alves <eliezeralvesbernardino@gmail.com>
2022-10-05 22:33:26 -0300

    Argumento Out

---
+++ CursoProgramacaoCSharp/Aula24_ArgumentoOut/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+class Aula24
+{
+    static void Main(){
+        int divid, divis,quoc,rest;
+        divid = 10;
+        divis = 5;
+        quoc = divide(divid,divis,out rest);
+
+        Console.WriteLine($"{divid} / {divis}: quociente = {quoc} e resto = {rest}");
+    }
+    static int divide(int dividendo, int divisor, out int resto){
+        int quociente;
+        quociente = dividendo/divisor;
+        resto = dividendo % divisor;
+        return quociente;
+    }
+        
+
+}
