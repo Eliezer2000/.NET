3209cd4d790772e2b7bf7a98dc6aca8b85a6c483
Eliezer Alves <eliezeralvesbernardino@gmail.com>
2022-10-05 21:57:34 -0300

    Passagem por valor e por referência

---
+++ CursoProgramacaoCSharp/Aula23_PassagemPorValorEReferencia/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+class Aula23{
+    static void Main(){
+        int num = 50;
+        dobrar2(num);
+        Console.WriteLine(num);
+    }
+    static void dobrar1(ref int valor){
+        valor *= 2;
+    }
+
+    static void dobrar2( int valor){
+        valor *= 2;
+    }
+}
