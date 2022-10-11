9204e7ea16e02e371252b6161f1942b72fde5e3a
Eliezer Alves <eliezeralvesbernardino@gmail.com>
2022-10-10 23:15:57 -0300

    Herança Ordem de execução dos construtores

---
+++ CursoProgramacaoCSharp/Aula35_HerancaOrdemExecucaoConstrutores/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+class Base{
+
+    public Base(){
+        Console.WriteLine("Construtor da classe Base");
+    }
+}
+class Derivada1:Base{
+
+    public Derivada1(){
+        Console.WriteLine("Construtor da classe Derivada1");
+    }
+}
+class Derivada2:Derivada1{
+
+    public Derivada2(){
+        Console.WriteLine("Construtor da classe Derivada2");
+    }
+}
+
+
+
+class Aula35{
+    static void Main(){
+        Derivada2 derivada2 = new Derivada2();
+    }
+}
