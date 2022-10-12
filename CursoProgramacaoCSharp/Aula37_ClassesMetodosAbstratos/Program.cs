a8d268f38f6fcc0fa59f422efcdefef402c46f35
Eliezer Alves <eliezeralvesbernardino@gmail.com>
2022-10-11 22:57:06 -0300

    Classes e métodos abstratos

---
+++ CursoProgramacaoCSharp/Aula37_ClassesMetodosAbstratos/Program.cs
@@ -0,0 +1,34 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+abstract class Veiculo{ //Classe base abstrata
+    protected int velMaxima;
+    protected int velAtual;
+    protected bool ligado;
+    public Veiculo(){
+        ligado = false;
+        velAtual = 0;
+    }
+    public void setLigado(bool ligado){
+        this.ligado = ligado;
+    }
+    public int getVelAtual(){
+        return velAtual;
+    }
+    abstract public void aceleracao(int mult);
+}
+class Carro:Veiculo{
+    public Carro(){
+        velMaxima = 120;
+    }
+    override public void aceleracao(int mult){
+        velAtual += 10*mult;
+    }
+}
+class Aula37
+{
+    static void Main(){
+        Carro carro = new Carro();
+        carro.aceleracao(1);
+        Console.WriteLine(carro.getVelAtual());
+    }
+}
