91b7a05a3dadabc0e50c065bd59485b3f7cdcc5a
Eliezer Alves <eliezeralvesbernardino@gmail.com>
2022-10-12 21:47:49 -0300

    Classe Sealed

---
+++ CursoProgramacaoCSharp/Aula38_ClasseSealed/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+sealed  class Veiculo{
+
+}
+class Carro:Veiculo{
+    //Classes sealed não podem ser seladas
+}
+
+class Aula38
+{
+    static void Main(){
+        Carro c1 = new Carro();
+
+    }
+}
