0c98df3046683a936c1e6a25caab400b17092a8e
Eliezer Alves <eliezeralvesbernardino@gmail.com>
2022-10-12 22:36:45 -0300

    Acessors Get e Set

---
+++ CursoProgramacaoCSharp/Aula39_AcessorsGet-Set/Program.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+class Carro{
+    private int velMax;
+
+    public int vm{
+        get{
+            return velMax;
+        }
+        set{
+            if(value < 0){
+                velMax = 0;
+            } else if(value > 300){
+                velMax = 300;
+            }else{
+                velMax = value;
+            }
+        }
+    }
+
+    public Carro(){
+        vm = 120;
+    }
+}
+
+class Aula39{
+    static void Main(){
+        Carro c1 = new Carro();
+
+        c1.vm = 200;
+        Console.WriteLine($"Velocidade {c1.vm}");
+    }
+}
