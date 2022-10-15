b4ec0c31060304f69a48f3835e674bafbafbcc3c
Eliezer Alves <eliezeralvesbernardino@gmail.com>
2022-10-14 21:47:39 -0300

    Struct

---
+++ CursoProgramacaoCSharp/Aula42_Struct/Program.cs
@@ -0,0 +1,34 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+struct Carro{
+    public string marca;
+    public string modelo;
+    public string cor;
+    public Carro(string marca, string modelo, string cor){
+        this.marca = marca;
+        this.modelo = modelo;
+        this.cor = cor;
+    }
+    public void Info(){
+        Console.WriteLine($"Marca:....{this.marca}");
+        Console.WriteLine($"Modelo:...{this.modelo}");
+        Console.WriteLine($"Cor:......{this.cor}");
+        Console.WriteLine("==========================");
+    }
+}
+
+
+class Aula42
+{
+    static void Main(){
+        Carro c1;
+        c1.marca = "Vw";
+        c1.modelo = "Golf";
+        c1.cor = "Branco";
+
+        Carro c2 = new Carro("Honda","MRV","Prata");
+
+        c1.Info();
+        c2.Info();
+    }
+}
