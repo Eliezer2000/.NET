294e5ebf497742f749f21834f8590af3db2c5763
Eliezer Alves <eliezeralvesbernardino@gmail.com>
2022-10-24 09:50:29 -0300

    Métodos e variáveis estáticos

---
+++ CursoProgramacaoCSharp/Aula47_MetodosVariaveisEstaticos/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+class Mat{
+    public static double pi = 3.14;
+
+    public static int dobro(int n){
+        return n * 2;
+    }
+}
+
+class Aula47
+{
+    static void Main(){
+        double vpi = Mat.pi;
+        int num = 10;
+
+        Console.WriteLine(vpi);
+        Console.WriteLine($"{Mat.dobro(num)}");
+    }
+}
