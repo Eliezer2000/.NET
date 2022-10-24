64abb020bebea044c9bee4f3ef356243e98aa2af
Eliezer Alves <eliezeralvesbernardino@gmail.com>
2022-10-24 14:37:44 -0300

    Namespaces

---
+++ CursoProgramacaoCSharp/Aula51_Namespaces/Program.cs
@@ -0,0 +1,59 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+namespace Calc1
+{
+    class Area
+    {
+        public static float quad(float bas, float alt)
+        {
+            if (bas == 0 || alt == 0)
+            {
+                throw new Exception("Base ou altura não podem ser igual a 0");
+            }
+            return bas * alt;
+        }
+    }
+}
+namespace Calc2
+{
+    class Area
+    {
+        public static float quad(float bas, float alt)
+        {
+            if (bas == 0 || alt == 0)
+            {
+                throw new Exception("Base ou altura não podem ser igual a 0");
+            }
+            return bas * alt;
+        }
+    }
+}
+
+
+class Aula51
+{
+    static void Main()
+    {
+
+        float area = 0;
+
+
+        try
+        {
+            area = Calc1.Area.quad(0, 5f);
+            Console.WriteLine($"Area do quadrado {area}");
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine($"ERRO: {e.Message}");
+
+        }
+        finally
+        {
+            Console.WriteLine("Fim do processo");
+        }
+
+
+
+    }
+}
