276e48a7322e28b16de6c2e2fe0721ab68fd6ed1
Eliezer Alves <eliezeralvesbernardino@gmail.com>
2022-10-24 11:23:29 -0300

    Argumentos de entrada do programa

---
+++ CursoProgramacaoCSharp/Aula49_ArgumentosEntradaPrograma/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+class Aula49
+{
+    static void Main(string[] args){
+
+        int res = 0;
+
+        if(args.Length > 0){
+            Console.WriteLine($"Qtde de argumentos {args.Length}");
+            for(int i=0; i < args.Length; i++){
+                res += Convert.ToInt32(args[i]);
+            }
+            Console.WriteLine($"Soma: {res}");
+        }else{
+            Console.WriteLine("Não foram passados os argumentos");
+        }
+    }
+}
